b4fed8581fdf575082bb75448cf70af8e83a5c1c
prasanth2817 <prasanth2817@gmail.com>
2024-10-30 10:48:57 +0530

    updated CORS

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ dotenv.config();
 const app= express();
 const PORT= process.env.PORT || 8000
 
-app.use(cors());
-app.use(morgan("combined"));
-app.use(morgan(":method :url :status :response-time ms"));
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+      credentials: true,
+    })
+  );
+  
 app.use(morgan("dev"));
 app.use(express.json())
 app.use("/",AppRoutes)
